a9b1a433a8adb730e4810840c4f5de667645ca22
PierreSOREAU <soreaupierre@gmail.com>
2026-05-15 01:43:57 +0200

    chore-configuration de l'environnement et du .gitignore

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+//express c'est le framework pour node.js. Node.js c'est le moteur qui permet de lire le js 
+//et d'obtenir des réactions sur l'ordinateur par exemple accéder 
+//à une base de données ou créer une API. mais sans express c'est hyper galère, 
+// express apporte un cadre de travail 
+// et des outils plus rapide pour obtenir finalement les même résultats
+//ici on importe donc le framework 
+
+import express from "express";
+
+const app = express();
+
+export default app;
---
+++ backend/src/config/constants.js
@@ -0,0 +1,3 @@
+//permet de stocker des données qui puissent être   
+// réutilisées en surnom plus facile à écrire 
+// pour d’autres fichier de l’architecture
