35f13c66dc8d0a454bfb894d30d47efe06b1f2cb
PierreSOREAU <soreaupierre@gmail.com>
2026-05-25 23:06:49 +0200

    style-ajout responsive design sur login et register

--- frontend/src/app/features/authentification/forgot-password/forgot-password.ts
+++ frontend/src/app/features/authentification/forgot-password/forgot-password.ts
@@ -4,10 +4,13 @@ import { InputComponent } from '../../../_shared/input/input';
 import { AuthService, LoginCredentials } from '../services/auth.services';
 import { Title } from '@angular/platform-browser';
 import { Router, RouterLink } from '@angular/router';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { getFieldErrorMessage } from '../../../_shared/utils/forms-error';
+
 
 @Component({
   selector: 'app-forgot-password',
-  imports: [ButtonRecord, InputComponent, RouterLink],
+  imports: [ButtonRecord, InputComponent, RouterLink, ReactiveFormsModule],
   templateUrl: './forgot-password.html',
   styleUrl: './forgot-password.scss',
 })
@@ -18,6 +21,20 @@ export class ForgotPassword implements OnInit {
     this.forgotPasswordTitle.setTitle('Coloc&Me | Mot de passe oublié')
 
 
+  }
+
+  forgotPasswordForm: FormGroup
+
+  constructor(private fb: FormBuilder) {
+    this.forgotPasswordForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+    })
+  }
+
+  getErrorMessage(textField: string, nameField: string): string {
+    const control = this.forgotPasswordForm.get(nameField);
+
+    return getFieldErrorMessage(textField, control);
   }
 
 }
